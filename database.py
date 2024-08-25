20bea078b9bdedae92c70027ad5946defdd485ff
himanshumishra1309 <himanshu.mishra0913@gmail.com>
2024-08-25 19:52:32 +0530

    added a hosted database url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:13092005@localhost/College_Website"
+SQLALCHEMY_DATABASE_URL = "postgresql://devs:JjOnnRFYM7YHzRSaEXkq52OmjYsj6jdA@dpg-cr527252ng1s73e9t6e0-a/college_website_ltzw"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
